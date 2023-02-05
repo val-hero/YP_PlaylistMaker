2b2e19ac6f5274579b7a3f2d0b34cb8b6df90a81
valhero <valhero.lmp@gmail.com>
2023-02-05 23:36:16 +0300

    Buttons on main screen are clickable now, showing specific text on click. I used both methods: lambda and anonymous class.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.song_search_button)
+        val searchClick: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Search button is pressed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        searchButton.setOnClickListener(searchClick)
+
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Library button is pressed", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Settings button is pressed", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
