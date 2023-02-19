ca1f92c51d35686d2a33a1dfc6cd796e3855223f
valhero <valhero.lmp@gmail.com>
2023-02-19 19:48:51 +0300

    Working with styles

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,46 +1,68 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.ContentInfo
+import android.view.InputDevice
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import android.widget.FrameLayout
 import android.widget.ImageView
 import com.example.playlistmaker.R.*
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_EDIT_TEXT = "SEARCH_EDIT_TEXT"
+    }
+
+    private lateinit var searchField: EditText
+    private lateinit var searchEditText: CharSequence
+    private lateinit var clearButton: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_search)
-
-        val inputField = findViewById<EditText>(id.input_field)
-        val clearButton = findViewById<ImageView>(id.clear_button)
+        searchField = findViewById(id.search_field)
+        clearButton = findViewById(id.clear_button)
 
         clearButton.setOnClickListener {
-            inputField.setText("")
+            searchField.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+            inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+            searchField.clearFocus()
         }
 
         val textWatcher = object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearButton.visibility = clearButtonVisibility(text)
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                clearButton.visibility = clearButtonVisibility(p0)
+            override fun afterTextChanged(text: Editable?) {
+                searchEditText = text!!
             }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
         }
-        inputField.addTextChangedListener(textWatcher)
+        searchField.addTextChangedListener(textWatcher)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putCharSequence(SEARCH_EDIT_TEXT, searchEditText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchEditText = savedInstanceState.getCharSequence(SEARCH_EDIT_TEXT, "")
+        searchField.setText(searchEditText)
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if(s.isNullOrEmpty()) {
+        return if (s.isNullOrEmpty()) {
             View.GONE
         } else
             View.VISIBLE
