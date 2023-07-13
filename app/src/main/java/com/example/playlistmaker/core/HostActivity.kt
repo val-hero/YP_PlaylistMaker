b41151e7326ffbf3eecdb94835f966386f4f0748
valhero <valhero.lmp@gmail.com>
2023-07-13 17:06:20 +0300

    refactor: change settings activity to fragment

--- app/src/main/java/com/example/playlistmaker/core/HostActivity.kt
+++ app/src/main/java/com/example/playlistmaker/core/HostActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker.core
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.R
 
-class HostActivity: AppCompatActivity()
+class HostActivity: AppCompatActivity(R.layout.activity_host)
