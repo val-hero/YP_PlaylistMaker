71aff073b6c0196db2b65fc59aa272a84113b117
valhero <valhero.lmp@gmail.com>
2023-07-14 17:18:16 +0300

    feat: add bottom nav view, setup nav graph

--- app/src/main/java/com/example/playlistmaker/core/HostActivity.kt
+++ app/src/main/java/com/example/playlistmaker/core/HostActivity.kt
@@ -1,6 +1,25 @@
 package com.example.playlistmaker.core
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityHostBinding
 
-class HostActivity: AppCompatActivity(R.layout.activity_host)
+class HostActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHostBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+}
