e6d5ffc11c90b9bc1f3f06f68a165d4bddd464f3
valhero <valhero.lmp@gmail.com>
2023-09-21 01:51:30 +0300

    fix inconsistent colors in input fields

--- app/src/main/java/com/example/playlistmaker/create_playlist/CreatePlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/create_playlist/CreatePlaylistFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.playlistmaker.databinding.FragmentCreatePlaylistBinding
 
 class CreatePlaylistFragment : Fragment() {
@@ -17,5 +18,13 @@ class CreatePlaylistFragment : Fragment() {
     ): View? {
         binding = FragmentCreatePlaylistBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createPlaylistToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,6 +21,14 @@ class PlaylistsFragment : Fragment() {
     ): View {
         binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addPlaylistButton.setOnClickListener {
+            findNavController().navigate(R.id.action_libraryFragment_to_createPlaylistFragment)
+        }
     }
 
 
